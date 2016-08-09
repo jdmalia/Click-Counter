4e0f10486393cae1e7a89b1fc54d21dae0d8fec4
Jason Malia <jmalia3@gatech.edu>
2016-08-09 15:58:56 -0400

    Add second label and decrement button

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -11,33 +11,57 @@ import UIKit
 class ViewController: UIViewController {
     
     var count:Int = 0
-    var counterLabel:UILabel!
+    var counterLabelA:UILabel!
+    var counterLabelB:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Counter label
-        let label = UILabel()
-        label.frame = CGRectMake(150, 150, 60, 60)
-        label.text = "0"
-        self.view.addSubview(label)
-        self.counterLabel = label
+        // Counter label A
+        let labelA = UILabel()
+        labelA.frame = CGRectMake(150, 150, 60, 60)
+        labelA.text = "0"
+        self.view.addSubview(labelA)
+        self.counterLabelA = labelA
         
-        // Counter Button
-        let button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
+        // Counter label B
+        let labelB = UILabel()
+        labelB.frame = CGRectMake(300, 150, 60, 60)
+        labelB.text = "0"
+        self.view.addSubview(labelB)
+        self.counterLabelB = labelB
+        
+        // Increment Button
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 150, 60)
+        button.setTitle("Increment", forState: .Normal)
         button.setTitleColor(UIColor.blueColor(), forState: .Normal)
         self.view.addSubview(button)
         // Add target action to button
         button.addTarget(self, action: #selector(ViewController.incrementCount), forControlEvents: UIControlEvents.TouchUpInside)
         
+        // Decrement Button
+        button = UIButton()
+        button.frame = CGRectMake(150, 350, 150, 60)
+        button.setTitle("Decrement", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        // Add target action to button
+        button.addTarget(self, action: #selector(ViewController.decrementCount), forControlEvents: UIControlEvents.TouchUpInside)
+        
     }
     
     func incrementCount() {
         self.count += 1
-        self.counterLabel.text = "\(self.count)"
+        self.counterLabelA.text = "\(self.count)"
+        self.counterLabelB.text = "\(self.count)"
+    }
+    
+    func decrementCount() {
+        self.count -= 1
+        self.counterLabelA.text = "\(self.count)"
+        self.counterLabelB.text = "\(self.count)"
     }
 
 }
