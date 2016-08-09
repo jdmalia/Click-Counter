64f0fefa53603d6d969ccb12a78a7a3bb54f1620
Jason Malia <jmalia3@gatech.edu>
2016-08-09 15:48:58 -0400

    Add counter button and label

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count:Int = 0
+    var counterLabel:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Counter label
+        let label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        self.counterLabel = label
+        
+        // Counter Button
+        let button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func incrementCount() {
+        self.count += 1
+        self.counterLabel.text = "\(self.count)"
     }
-
 
 }
 
